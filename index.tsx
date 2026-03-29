9ef6477bfd4565c7756a910c6a5ab6b7a29d2dbc
jay-nuclear-phd <jeongwon.seo@austin.utexas.edu>
2026-03-29 18:46:50 -0500

    Fix router basename for GitHub Pages

--- index.tsx
+++ index.tsx
@@ -12,7 +12,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <LanguageProvider>
         <App />
       </LanguageProvider>
